32ee9277c76d50f6e6926b2bb006b0f9c207509f
Akshay Bhiogade <akshaybhiogade55@gmail.com>
2026-09-03 19:23:22 +0530

    Refactor server.js to use dynamic PORT variabledeployment step

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDb = require("./src/config/database")
+require("dotenv").config();
 
+const app = require("./src/app");
+const connectToDb = require("./src/config/database");
 
- 
-connectToDb()
+connectToDb();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {  
-    console.log("server is running on port 3000") 
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
